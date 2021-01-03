41eb46cb764ab0fbc12eb751212892c8f1fe3f55
최성진 <45679609+csj7480@users.noreply.github.com>
2021-01-03 23:54:13 +0900

    Create Level1_C++_divisor.cpp

---
+++ Programmers/Level1_C++_divisor.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); i++) {
+        if (!(arr[i] % divisor) || (divisor == 1)) {
+            answer.emplace_back(arr[i]);
+        }
+    }
+    if (answer.empty())
+        answer.emplace_back(-1);
+    sort(answer.begin(), answer.end());
+    return answer;
+}
