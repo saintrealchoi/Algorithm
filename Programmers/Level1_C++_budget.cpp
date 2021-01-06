443e4a2b55daa1bfc7f8ead01e909829304d0434
최성진 <45679609+csj7480@users.noreply.github.com>
2021-01-06 16:12:52 +0900

    Create Level1_C++_budget.cpp

---
+++ Programmers/Level1_C++_budget.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+
+    sort(d.begin(), d.end());
+
+    for (int i = 0; i < d.size(); i++) {
+        if (budget - d[i] >= 0) {
+            budget -= d[i];
+            answer++;
+        }
+        if (budget < 0)
+            break;
+    }
+    
+
+
+    return answer;
+}
