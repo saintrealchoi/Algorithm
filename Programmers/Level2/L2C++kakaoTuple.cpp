1dff7aca6e7939374d40da5aec6201e838e55b43
최성진 <45679609+csj7480@users.noreply.github.com>
2021-01-08 16:20:22 +0900

    Update L2C++kakaoTuple.cpp

--- Programmers/Level2/L2C++kakaoTuple.cpp
+++ Programmers/Level2/L2C++kakaoTuple.cpp
@@ -1,19 +1,37 @@
 #include <string>
 #include <vector>
-#include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(string s) {
-    vector<int> answer;
-    return answer;
-}
-
-int main() {
-    string s = "{{}{4}{}3{}2{}1}";
-    int size = s.size();
-    for (int i = 0; i < size; i++) {
-        s.erase(s.find('{'), 1);
+    vector<int> answer, v;
+    for (int i = 1; i < s.size() - 1; i++)
+    {
+        int sum = 0;
+        if (s[i] == '{')
+        {
+            int idx = i + 1;
+            for (int j = idx; j < s.size() - 1; j++)
+            {
+                if (s[j] == ',')
+                {
+                    sum += (stoi(s.substr(idx, j - idx)));
+                    idx = j + 1;
+                }
+                if (s[j] == '}')
+                {
+                    sum += (stoi(s.substr(idx, j - idx)));
+                    idx = j + 1;
+                    break;
+                }
+            }
+            v.push_back(sum);
+        }
     }
-    cout << s;
+
+    sort(v.begin(), v.end());
+    answer.push_back(v[0]);
+    for (int i = 1; i < v.size(); i++)
+        answer.push_back(v[i] - v[i - 1]);
+    return answer;
 }
