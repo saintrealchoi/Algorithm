fadd5af7aca3498a4224a57d1b4a9d9237206387
sungjinchoi <csj7480@naver.com>
2021-02-12 00:34:20 +0900

    Update BT_wordSearch.cpp
    Faster than 97%

--- Leetcode/BT_wordSearch.cpp
+++ Leetcode/BT_wordSearch.cpp
@@ -1,39 +1,49 @@
-class Solution {
+class Solution
+{
 public:
-    bool issafe(vector<vector<char>>& board, int i, int j, string word, int k) {
-        if (board[i][j] == word[k]) {
-            return true;
-        }
-        return false;
-    }
+    bool backTracking(vector<vector<char>>& board, int row, int col, string& target, int index) {
+        if (index == target.length() - 1) return true;
 
-    bool solve(vector<vector<char>>& board, int i, int j, int m, int n, string word, int k) {
-        if (k == word.size()) {
-            return true;
-        }
-        if (i < 0 || i == m || j < 0 || j == n) {
-            return false;
-        }
-        if (issafe(board, i, j, word, k)) {
-            char temp = board[i][j];
-            board[i][j] = '0';
-            if ((solve(board, i + 1, j, m, n, word, k + 1) || solve(board, i - 1, j, m, n, word, k + 1) || solve(board, i, j + 1, m, n, word, k + 1) || solve(board, i, j - 1, m, n, word, k + 1))) {
+        char tmp = board[row][col];
+        board[row][col] = '*';
+
+        if (row + 1 < board.size() && target[index + 1] == board[row + 1][col]) {
+            if (backTracking(board, row + 1, col, target, index + 1)) {
                 return true;
             }
-            //backtracking
-            board[i][j] = temp;
         }
 
+        if (col + 1 < board[0].size() && target[index + 1] == board[row][col + 1]) {
+            if (backTracking(board, row, col + 1, target, index + 1)) {
+                return true;
+            }
+        }
+
+        if (row - 1 >= 0 && target[index + 1] == board[row - 1][col]) {
+            if (backTracking(board, row - 1, col, target, index + 1)) {
+                return true;
+            }
+        }
+
+        if (col - 1 >= 0 && target[index + 1] == board[row][col - 1]) {
+            if (backTracking(board, row, col - 1, target, index + 1)) {
+                return true;
+            }
+        }
+
+        board[row][col] = tmp;
         return false;
     }
+
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        int k = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (solve(board, i, j, m, n, word, k)) {
-                    return true;
+        int i, j;
+
+        for (i = 0; i < board.size(); i++) {
+            for (j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == word[0]) {
+                    if (backTracking(board, i, j, word, 0)) {
+                        return true;
+                    }
                 }
             }
         }
